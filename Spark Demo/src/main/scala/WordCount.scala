23bdd089879f5768c16873db41fdf1a0133d508e
Shubhankar Satvaya <shubhankarsatvaya05@gmail.com>
2026-02-10 23:40:31 +0530

    Paired RDD and comment in Word Count file

--- Spark Demo/src/main/scala/WordCount.scala
+++ Spark Demo/src/main/scala/WordCount.scala
@@ -3,26 +3,47 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import java.io.File
 
-object WordeCount {
-  val inputFile = "D:\\Spark\\dataset\\word1.txt"
-  val outputFile = "D:\\Spark\\dataset\\output.txt"
+object WordCount {
+  val inputFile = "D:\\Spark_Trainings\\dataset\\word1.txt"
+  val outputFile = "D:\\Spark_Trainings\\dataset\\output.txt"
 
   def main(args: Array[String]): Unit = {
+    " Spark Configuration "
+
     val conf = new SparkConf().setMaster("local").setAppName("My App")
     val sc = new SparkContext(conf)
+    sc.setLogLevel("OFF")
 
-    val inputRDD: RDD[String] = sc.textFile(inputFile)
+    "Reading File with 3 partition"
+    val inputRDD: RDD[String] = sc.textFile(inputFile,3)
     println(s"Total Lines: ${inputRDD.count()} ")
     println(s" Total number of partition :${inputRDD.getNumPartitions}")
+
+    "Checking the number of Contents"
     val contentArr: Array[String] = inputRDD.collect()
-    println("content: ")
+    println("\n\n InputRDD collect Content: ")
     contentArr.foreach(println)
 
+    "Checking the data in each partition"
+    println("\n\nGlom Collect input RDD:")
+    inputRDD.glom().collect().foreach { partition =>
+      println(s"Partition: ${partition.mkString("[", ", ", "]")}")
+    }
+
+    "Getting each work , and counting the number of each word"
     val words: RDD[String] = inputRDD.flatMap(line => line split (" "))
     val countiPerWords: RDD[(String, Int)] = words.map(word => (word, 1))
     val counts: RDD[(String, Int)] = countiPerWords.reduceByKey{ (counter:Int, nextVal:Int) => counter + nextVal }
-    println("only collect",counts.collect())
-    println("Glom collect",counts.glom.count())
+
+    println("\n Only collect:")
+    counts.collect().foreach(println)
+
+    println("\nGlom collect:")
+    counts.glom.collect().foreach { partition =>
+      println(s"Partition: ${partition.mkString("[", ", ", "]")}")
+    }
+
+    "Writing output in each file, Each partition will create 1 file "
 //    FileUtils.deleteQuietly(new File(outputFile))
 //    counts.saveAsTextFile(outputFile)
 
